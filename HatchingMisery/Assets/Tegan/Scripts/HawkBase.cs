df466ef37f0425c53a28569460fb8c7a00c53ce6
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-02-27 23:55:23 +1100

    Created own folder; Created Hawk test scene; Created scripts for Hawk (Hawk currently hovers)

---
+++ HatchingMisery/Assets/Tegan/Scripts/HawkBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HawkBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public virtual void Enter()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public virtual void Execute()
+    {
+        
+    }
+
+    public virtual void Exit()
+    {
+        
+    }
+}
---
+++ HatchingMisery/Assets/Tegan/Scripts/HawkModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HawkModel : HawkBase
+{
+    public HawkBase swoopState;
+    public HawkBase patrolState;
+
+    public HawkBase currentState;
+
+
+    public void ChangeState(HawkBase newState)
+    {
+        newState.Enter();
+        currentState = newState;
+    }
+
+    private void Awake()
+    {
+        // On awake, activate patrol state (for now)
+        ChangeState(patrolState);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.Execute();
+        }
+    }
+}
