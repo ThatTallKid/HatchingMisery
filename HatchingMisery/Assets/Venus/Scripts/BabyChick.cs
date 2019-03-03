9062c5f00735b5d058e7bb2f8c63462e707a6013
Venus <inkyinked@gmail.com>
2019-03-03 23:34:11 +1100

    Added states for chicks; chicks now follow the mother hen, and stop when they find feed; they can also wander now.

---
+++ HatchingMisery/Assets/Venus/Scripts/BabyChick.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyChick : MonoBehaviour
+{
+    public float FollowSpeed;
+
+    public enum ChickState
+    {
+        Wandering,
+        Feeding,
+        Following
+    }
+    public ChickState CurrentState;    // Change back to private after
+    private GameObject MotherHen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentState = ChickState.Following;
+        MotherHen = FindObjectOfType<Chicken>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (CurrentState)
+        {
+            case ChickState.Following:
+                Vector3 ToChicken = MotherHen.transform.position - transform.position;
+                transform.Translate(ToChicken * FollowSpeed * Time.deltaTime);
+                break;
+            case ChickState.Feeding:
+                // Do whatever
+                break;
+            case ChickState.Wandering:
+                transform.Translate(new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2)) * FollowSpeed * Time.deltaTime);
+                break;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Feed>())
+        {
+            CurrentState = ChickState.Feeding;
+        }
+    }
+}
