7674b41da452bfbf899eea10073efdbd163a9065
Uncanny <notebucket@yahoo.com>
2019-03-11 21:38:58 +1100

    Simple placeholder camera and hen movement
    planned hen movement will rotate the hen to match movement direction and camera will be independent of the hen prefab

---
+++ HatchingMisery/Assets/Josh/CameraMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    public Vector3 offset;
+    public Vector3 Angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // to modify the camera positions until they are right these are both in update
+        // set the camera rotation to the desired rotation
+        transform.rotation.eulerAngles.Set(Angle.x,Angle.y,Angle.z);
+        // set the camera position to the player position + an offset where the player is assumed to be the parent
+        transform.position = transform.parent.position + offset;
+    }
+}
---
+++ HatchingMisery/Assets/Josh/HenMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HenMovement : MonoBehaviour
+{
+    public Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // only get the gameobject once rather than once per frame
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move in direction indicated by controller regardless of what the controller is
+        body.AddForce(Input.GetAxis("Vertical")/5,0,-Input.GetAxis("Horizontal")/5,ForceMode.VelocityChange);
+    }
+}
