77ead7c6c91c1ec6fca7db45dfce31ad70d5365b
Uncanny <notebucket@yahoo.com>
2019-03-14 21:37:49 +1100

    smooth camera, smooth movement avoidance, local scene variations again

--- HatchingMisery/Assets/Josh/CameraMotion.cs
+++ HatchingMisery/Assets/Josh/CameraMotion.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class CameraMotion : MonoBehaviour
 {
+    public GameObject target;
     public Vector3 offset;
     public Vector3 Angle;
+
+    private void Awake()
+    {
+        transform.SetParent(null);
+    }
 
     // Update is called once per frame
     void Update()
     {
         // to modify the camera positions until they are right these are both in update
         // set the camera rotation to the desired rotation
-        transform.rotation.eulerAngles.Set(Angle.x,Angle.y,Angle.z);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(Angle),Time.deltaTime);
+        //transform.rotation.eulerAngles.Set(Angle.x,Angle.y,Angle.z);
         // set the camera position to the player position + an offset where the player is assumed to be the parent
-        transform.position = transform.parent.position + offset;
+        
+        transform.position = Vector3.Lerp(transform.position,target.transform.position + offset,Time.deltaTime);
     }
 }
--- HatchingMisery/Assets/Josh/HenMovement.cs
+++ HatchingMisery/Assets/Josh/HenMovement.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HenMovement : MonoBehaviour
 {
     public Rigidbody body;
     public int chickamount;
     public GameObject ChickPrefab;
+
+    public NavMeshAgent Nav;
     // Start is called before the first frame update
     void Start()
     {
+        Nav = gameObject.GetComponent<NavMeshAgent>();
         // only get the gameobject once rather than once per frame
         body = gameObject.GetComponent<Rigidbody>();
         GameTime tempgame = FindObjectOfType<GameTime>();
@@ -37,7 +41,11 @@ public class HenMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Nav.speed = 5;
+        Nav.isStopped = false;
+        
+        Nav.destination = transform.position + new Vector3(Input.GetAxis("Vertical"),0,-Input.GetAxis("Horizontal")).normalized;
         // move in direction indicated by controller regardless of what the controller is
-        body.AddForce(Input.GetAxis("Vertical")/5,0,-Input.GetAxis("Horizontal")/5,ForceMode.VelocityChange);
+        //body.AddForce(Input.GetAxis("Vertical")/5,0,-Input.GetAxis("Horizontal")/5,ForceMode.VelocityChange);
     }
 }
