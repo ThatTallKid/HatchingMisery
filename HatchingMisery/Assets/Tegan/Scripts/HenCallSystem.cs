a96f0edd4470648072b3d620dbd8bea838feec76
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-03-26 23:22:20 +1100

    Working on a fix for the Hen's Call System (the states change in the editor, but not in the Chick's) (Also moved my Hawk scripts into a different folder as they're no longer being used)

---
+++ HatchingMisery/Assets/Tegan/Scripts/HenCallSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+
+public class HenCallSystem : MonoBehaviour
+{
+    public Button follow;
+    public Button stopFollow;
+
+    public BabyChickV2.ChickState currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        follow.onClick.AddListener(Follow);
+        stopFollow.onClick.AddListener(StopFollow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void Follow()
+    {
+        currentState = BabyChickV2.ChickState.Following;
+        Debug.Log("Chicks are following");
+    }
+
+    void StopFollow()
+    {
+        currentState = BabyChickV2.ChickState.Wandering;
+        Debug.Log("Chicks have stopped following");
+    }
+}
