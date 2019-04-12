6589d0109586f29ae219197317b68b6b193124ed
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2019-04-12 20:03:32 +1000

    HenCallSystemControls pushed (oops)

---
+++ HatchingMisery/Assets/Tegan/Scripts/HenCallSystemControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HenCallSystemControls
+{
+  // Call is Left Mouse/A button
+  public static bool AButton()
+  {
+    return Input.GetButtonDown("A_Button");
+  }
+  
+  // Stop is Right Mouse/B button
+  public static bool BButton()
+  {
+    return Input.GetButtonDown("B_Button");
+  }
+}
