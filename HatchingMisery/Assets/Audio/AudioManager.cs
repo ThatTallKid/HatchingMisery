223bd6be02381d77af26c8d8d80f0f381a6035f2
Ben <bennie-0@hotmail.com>
2019-04-14 23:35:50 +1000

    Added in SFX and music Manager

---
+++ HatchingMisery/Assets/Audio/AudioManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] musicTrackArray;
+    public AudioSource currentMusicTrack;
+    public bool playMusicTrack2;
+    public bool playMusicTrack3;
+    public HenMovement henMovementScript;
+    public int chicksLeft;
+    
+
+    void Awake()
+    {
+        currentMusicTrack = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //henMovementScript = GetComponents<HenMovement>(chickamount);
+        //chickamount = chicksLeft;
+
+        currentMusicTrack.clip = musicTrackArray[(0)];
+        currentMusicTrack.PlayOneShot(currentMusicTrack.clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playMusicTrack2 == true)
+        {
+            currentMusicTrack.Stop();
+
+            currentMusicTrack.clip = musicTrackArray[(1)];
+            currentMusicTrack.PlayOneShot(currentMusicTrack.clip);
+            currentMusicTrack.Play();
+        }
+
+        if (playMusicTrack3 == true)
+        {
+            currentMusicTrack.Stop();
+
+            currentMusicTrack.clip = musicTrackArray[(2)];
+            currentMusicTrack.PlayOneShot(currentMusicTrack.clip);
+            currentMusicTrack.Play();
+        }
+    }
+
+    public void PlayMusicTrack2()
+    {
+       if (chicksLeft <= 12)
+        {
+            playMusicTrack2 = true;
+        }
+        else
+        {
+            playMusicTrack2 = false;
+        }
+        
+    }
+
+    public void PlayMusicTrack3()
+    {
+        if (chicksLeft <= 5)
+        {
+            playMusicTrack3 = true;
+        }
+        else
+        {
+            playMusicTrack3 = false;
+        }
+        
+    }
+}
---
+++ HatchingMisery/Assets/Audio/SFXManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioClip[] sfxSoundsArray;
+    public AudioSource sfxSound;
+    public int randomSFX;
+    public float timeCounter;
+    public float randomTime;
+    public float rtMin;
+    public float rtMax;
+
+    void Awake()
+    {
+        sfxSound = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeCounter > randomTime)
+        {
+            randomTime = Random.Range(rtMin, rtMax);
+            timeCounter = 0f;
+            sfxSound.Stop();
+            ChooseMusic();
+            sfxSound.Play();
+        }
+        timeCounter += Time.deltaTime;
+    }
+
+    public void ChooseMusic()
+    {
+        randomSFX = Random.Range(0, 2);
+
+        switch (randomSFX)
+        {
+            case 0:
+                sfxSound.clip = sfxSoundsArray[(0)];
+                sfxSound.PlayOneShot(sfxSound.clip);
+                break;
+            case 1:
+                sfxSound.clip = sfxSoundsArray[(1)];
+                sfxSound.PlayOneShot(sfxSound.clip);
+                break;
+        }
+    }
+}
