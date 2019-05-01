1897eedea8a0286fa47cc9be7c49ccee30f58974
Murphy <1009894@student.sae.edu.au>
2019-05-01 22:28:12 +1000

    Final Touches

---
+++ HatchingMisery/Assets/Murphy/EndGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public float GameTime;
+    public bool trig;
+    public Image panskils;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panskils.CrossFadeAlpha(0f, 4f, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trig == true)
+        {
+            panskils.CrossFadeAlpha(4f, 4f, false);
+            GameTime = GameTime + Time.deltaTime * 1;
+        }
+
+        if (GameTime > 4)
+        {
+            SceneManager.LoadScene(9);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+
+        trig = true;
+    }
+
+
+}
